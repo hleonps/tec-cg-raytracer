e7b94de1863b39edda9fd8caee4a1be487a260f8
Heiner León <h.leon12p@gmail.com>
2016-10-10 21:47:50 -0600

    Add light source to the scene

--- headers/sphere.h
+++ headers/sphere.h
@@ -6,4 +6,5 @@ struct cgSphere {
 	cgPoint3f center;
 };
 
+cgVector3f cgSphereNormalVector(cgPoint3f intersection, void *information);
 cgIntersection * cgSphereIntersection(cgPoint3f camera, cgVector3f ray_direction, void * data);
--- src/scene.c
+++ src/scene.c
@@ -6,9 +6,10 @@
 #include "vector.h"
 #include "object.h"
 #include "sphere.h"
+#include "light_source.h"
 #include "scene.h"
 
-cgScene scene = {NULL, 0};
+cgScene scene = {NULL, 0, NULL, 0, 0};
 
 void cgAddSphereToScene(cgPoint3f center, long double radius, cgColor color){
 	// cgObject * sphere = (cgObject *) malloc(sizeof(cgObject));
@@ -16,6 +17,9 @@ void cgAddSphereToScene(cgPoint3f center, long double radius, cgColor color){
 	sphere.type = SPHERE;
 	sphere.color = color;
 	sphere.intersection = &cgSphereIntersection;
+	sphere.normal_vector = &cgSphereNormalVector;
+	sphere.diffuse_factor = 0.8;
+	sphere.environment_lighting = 0.2;
 
 	cgSphere * information = (cgSphere *) malloc(sizeof(cgSphere));
 	information->radius = radius;
@@ -27,6 +31,26 @@ void cgAddSphereToScene(cgPoint3f center, long double radius, cgColor color){
 
 	scene.objects = (cgObject *) realloc(scene.objects, sizeof(cgObject) * scene.num_objects);
 	scene.objects[scene.num_objects - 1] = sphere;
+}
+
+void cgAddLightSourceToScene(cgPoint3f position, long double intensity, long double c1, long double c2, long double c3){
+	cgLight light_source;
+
+	light_source.position = position;
+	light_source.intensity = intensity;
+
+	light_source.c1 = c1;
+	light_source.c2 = c2;
+	light_source.c3 = c3;
+
+	scene.num_lights++;
+
+	scene.lights = (cgLight *) realloc(scene.lights, sizeof(cgLight) * scene.num_lights);
+	scene.lights[scene.num_lights - 1] = light_source;
+}
+
+void cgSetEnvironmentLighting(long double intensity){
+	scene.environment_lighting = intensity;
 }
 
 cgIntersection * first_intersection(cgPoint3f camera, cgVector3f ray_direction){

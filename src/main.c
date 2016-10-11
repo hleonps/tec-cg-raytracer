2a2f6f856f2679a68f979a7288e3b235da0b13f6
Heiner León <h.leon12p@gmail.com>
2016-10-10 18:31:37 -0600

    Write AVS file

--- src/main.c
+++ src/main.c
@@ -1,15 +1,28 @@
+
+#include <stdio.h>
+#include <math.h>
 #include "vector.h"
 #include "projection.h"
 #include "graphics.h"
 #include "object.h"
 #include "scene.h"
 
+#define SWAP(x) ( ((x) << 24) | \
+	(((x) << 8) & 0x00ff0000) | \
+	(((x) >> 8) & 0x0000ff00) | \
+	((x) >> 24) )
+#define FIX(x) (*(unsigned *)&(x) = \
+	SWAP(*(unsigned *)&(x)))
+
 void generate_image(void);
 cgColor pick_color(cgPoint3f camera, cgVector3f ray_direction);
+void save_image();
+
+#define FILE_NAME "scene.avs"
 
 int main(int argc, char const *argv[]) {
 
-	init_framebuffer(640, 800);
+	init_framebuffer(800, 800);
 	cgSetProjectionMatrix(0, 0, 1000, 1000);
 	cgSetCamaraPosition(100, 200, -100);
 
@@ -29,6 +42,8 @@ void generate_image(){
 			framebuffer[i][j] = pick_color(camera, unit_ray_vector);
 		}
 	}
+
+	save_image();
 }
 
 cgColor pick_color(cgPoint3f camera, cgVector3f ray_direction){
@@ -42,4 +57,36 @@ cgColor pick_color(cgPoint3f camera, cgVector3f ray_direction){
 	}
 
 	return color;
+}
+
+void save_image(){
+	FILE * fp = fopen(FILE_NAME, "w");
+	/* Attempt to open the file */
+	if (fp == NULL) {
+		printf("failed to create file\n");
+	}
+	else{
+		// Store header
+		unsigned int width = SWAP(framebuffer_h);
+		unsigned int height = SWAP(framebuffer_v);
+		fwrite(&width, sizeof(unsigned int), 1, fp);
+		fwrite(&height, sizeof(unsigned int), 1, fp);
+
+		for(int i = 0; i < framebuffer_h; i++){
+			for(int j = 0; j < framebuffer_v; j++){
+				/* Write the current pixel */
+				unsigned char alpha = round(framebuffer[i][j].a * 255);
+				unsigned char red = round(framebuffer[i][j].r * 255);
+				unsigned char green = round(framebuffer[i][j].g * 255);
+				unsigned char blue = round(framebuffer[i][j].b * 255);
+
+				fwrite(&alpha, sizeof(unsigned char), 1, fp);
+				fwrite(&red, sizeof(unsigned char), 1, fp);
+				fwrite(&green, sizeof(unsigned char), 1, fp);
+				fwrite(&blue, sizeof(unsigned char), 1, fp);
+			}
+		}
+
+		fclose(fp);
+	}
 }

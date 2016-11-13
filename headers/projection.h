8638753246a1c937d9b4f373c45e6729d3bcc9d3
Heiner León <h.leon12p@gmail.com>
2016-11-13 09:30:37 -0600

    Static Antialiasing: sample of 4

--- headers/projection.h
+++ headers/projection.h
@@ -8,5 +8,5 @@ cgPoint3f camera;
 
 void cgSetCameraPosition(int x, int y, int z);
 void cgSetProjectionMatrix(int left, int right, int bottom, int top);
-long double cgMapXFrameToProjectionMatrix(int x);
-long double cgMapYFrameToProjectionMatrix(int y);
+long double cgMapXFrameToProjectionMatrix(int x, long double offset);
+long double cgMapYFrameToProjectionMatrix(int y, long double offset);
